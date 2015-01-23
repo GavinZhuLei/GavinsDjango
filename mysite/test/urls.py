6b3fe9808c9be3912374a07cc834e62b554433a5
Gavin <gavin42333@hotmail.com>
2015-01-23 19:54:04 +0800

    加入测试页面

--- mysite/test/urls.py
+++ mysite/test/urls.py
@@ -1,11 +1,11 @@
+#__author__ = 'Gavin'
+
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    #url(r'^admin/', include(admin.site.urls)),
-    url(r'^test/',include('test.urls')),
+    url(r'^$','test.views.index',name='index'),
 )
